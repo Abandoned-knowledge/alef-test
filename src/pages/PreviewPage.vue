8836ffbae8554d12cafda0ce9877e870fcfb639a
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-11-08 20:30:24 +0300

    complete PreviewPage

--- src/pages/PreviewPage.vue
+++ src/pages/PreviewPage.vue
@@ -1,8 +1,27 @@
 <script setup lang="ts">
 import HeaderBase from "@/components/HeaderBase.vue";
+import useChildrenStore from "@/shared/stores/children";
+import usePersonalStore from "@/shared/stores/personal";
+const personalStore = usePersonalStore();
+const childrenStore = useChildrenStore();
 </script>
 
 <template>
   <HeaderBase />
-  <h1>preview</h1>
+  <main class="container mt-8 flex w-[800px] flex-col items-center gap-8">
+    <div class="w-full">
+      <p>Персональные данные</p>
+      <p class="mt-5 font-bold">{{ personalStore.getFullName }}</p>
+    </div>
+
+    <div class="flex w-full flex-col gap-5">
+      <p>Дети</p>
+      <div
+        class="w-fit rounded bg-gray-light px-5 py-3"
+        v-for="child in childrenStore.children"
+      >
+        <p class="font-bold">{{ child.name }}, {{ child.age }} лет</p>
+      </div>
+    </div>
+  </main>
 </template>
--- src/shared/stores/personal.ts
+++ src/shared/stores/personal.ts
@@ -1,11 +1,13 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 const usePersonalStore = defineStore("personal", () => {
   const name = ref<string>("Пустое");
   const age = ref<number>(0);
 
-  return { name, age };
+  const getFullName = computed(() => `${name.value}, ${age.value} лет`);
+
+  return { name, age, getFullName };
 });
 
 export default usePersonalStore;
