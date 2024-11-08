0a2cd778f8265473d4472b3b4942fdf637ca9546
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-11-08 12:28:31 +0300

    install font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: '"Montserrat", serif',
+    },
   },
   plugins: [],
 };
